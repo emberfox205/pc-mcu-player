e139a1e1b065d43c7f97d3e1bf521691daf416ef
Phuc Doan <89180888+emberfox205@users.noreply.github.com>
2025-02-14 23:22:12 +0700

    Experimented with FreeRTOS and SMP.

--- pc-client/client.py
+++ pc-client/client.py
@@ -1,16 +1,17 @@
 from PIL import Image 
-import serial, time
+import serial
 
-CHUNK_SIZE = 256
-FRAME_FIRST = 5431
-FRAME_LAST = 5478
+FRAME_FIRST = 43
+FRAME_LAST = 6518
 
 # Connect to simulation using RFC2217 through port 4000
-ser = serial.serial_for_url('rfc2217://localhost:4000', baudrate=921600)
+# Baud rate must be consistent with C++ embedded codes
+ser = serial.serial_for_url('rfc2217://localhost:4000', baudrate=115200, timeout=5)
 
 # Import frames
 img_array = list()
 for index in range(FRAME_FIRST, FRAME_LAST + 1): 
+    # This is just my own naming scheme
     img = Image.open(f"badapple/xbm_frames/frame_{index:04}.xbm")
     img_array.append(img)
 
@@ -29,28 +30,20 @@ for img in img_array:
 
 # Wait for signal to confirm setup is done 
 while True: 
-    frame_signal = ser.read_until('F', 1).decode("utf-8").rstrip()
-    if (frame_signal == 'F'):
-        print("Frame buffer ready")
+    frame_signal = ser.read_until('S',1).decode("utf-8").rstrip()
+    if (frame_signal == 'S'):
+        print("MCU ready")
         break
 
-start = time.time()
+# Loop through each byte array
 for bytes_frame in img_bytes_array:
-    # Send bytes in chunks.
-    for i in range(0, len(bytes_frame), CHUNK_SIZE):
-        chunk = bytes_frame[i:i+CHUNK_SIZE]
-        # Add an end marker for final chunk of frame
-        if (len(chunk) < CHUNK_SIZE):
-            chunk += b'\n'
-        ser.write(chunk)
-        # Wait for signal to confirm chunk is received and stored
-        while True:
-            chunk_signal = ser.read_until('C',1).decode("utf-8").rstrip()
-            if (chunk_signal == 'C'):
-                print("Chunk arrived")
-                break
-
-end = time.time()
+    ser.write(bytes_frame)
+    # Wait for confirming signal before continue sending
+    while True:
+        chunk_signal = ser.read_until('F',1).decode("utf-8").rstrip()
+        if (chunk_signal == 'F'):
+            print("Frame arrived")
+            break
+    
 # Disconnect
 ser.close()
-print(f"FPS = {(FRAME_LAST - FRAME_FIRST)/(end-start)}")
--- src/main.cpp
+++ src/main.cpp
@@ -2,85 +2,128 @@
 #include <Wire.h>
 #include "U8g2lib.h"
 
-// 256 is the maximum rx buffer size
-// Controller's BYTE_CHUNK being SLIGHTLY smaller than pc's CHUNK_SIZE
-// helps minimize delay
-const uint16_t BYTE_CHUNK = 240; 
-const uint16_t IMG_HEIGHT = 64;
-const uint16_t IMG_WIDTH = 88; // Must be divisible by 8
+// Baud rate must be consistent between this and the Python client
+const uint32_t BAUD_RATE = 115200;
+
+// Image dimensions 
+const uint16_t IMG_HEIGHT = 64;     
+const uint16_t IMG_WIDTH = 88;      // Must be divisible by 8
 const uint16_t IMG_BYTES = ((IMG_WIDTH * IMG_HEIGHT) / 8);
-const uint8_t DELAY = 1;
-const uint8_t DELAY_OFFSET = 8; // Pad delay to wait for data
-const uint8_t X_COORD = 21;
+
+// Position (upper-left corner) of the animation on the OLED screen
+const uint8_t X_COORD = 21;         
 const uint8_t Y_COORD = 0;
 
-uint8_t imageArray[IMG_BYTES];
-uint8_t byteArray[BYTE_CHUNK];
+// Serial receive buffer size
+const uint16_t RX_BUFFER_SIZE = 1024;
 
-int bytePos = 0; // Track where new bytes are being added to imageArray
-int bytesRead = 0;
-bool isReadyToReceive = false;
+// Determine number of buffer frames
+const uint16_t IMG_RACK = 32;       
 
-// Display Constructor 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
+// (Unused) Track number of bytes read by Serial.readBytes()
+static uint16_t bytesRead = 0;
+
+// Index to track which frame slot is being written to 
+uint16_t currentWriteFrame = 0; 
+// Index to determine frame in imageArray to display
+uint16_t currentDisplayFrame = 0;      
+
+// Status of the frames held in imageArray
+bool imageStatus[IMG_RACK] = {false}; 
+uint8_t imageArray[IMG_RACK][IMG_BYTES]; // Ring buffer
+
+// Task management
+TaskHandle_t serialHandle = NULL;
+TaskHandle_t displayHandle = NULL;
+
+// For signaling setup() connection on initial port connection
+bool isReady = false;             
+const uint8_t DELAY = 5;
+
+// Display Constructor
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0); 
+
+void serialProcess(void * parameter) {
+    for (;;) {
+        // Signal that setup() is completed, as it takes a long time on initial start
+        if (!isReady) {
+            delay(DELAY);
+            Serial.write('S');
+        }
+
+        while (Serial.available()) {
+            isReady = true;
+            bytesRead = Serial.readBytes(imageArray[currentWriteFrame], IMG_BYTES);
+            imageStatus[currentWriteFrame] = 1;
+            // Wait so as not to overwrite written frames
+            uint16_t nextWriteFrame = (currentWriteFrame + 1) % IMG_RACK;
+            while (imageStatus[nextWriteFrame]) {
+                taskYIELD();
+            }
+            currentWriteFrame = nextWriteFrame;
+            Serial.write("F");
+        }
+        taskYIELD();
+    }
+}
+
+void display(void * parameter) {
+    for (;;) {
+        if (imageStatus[currentDisplayFrame]) {
+            u8g2.clearBuffer();
+            u8g2.drawXBM(
+                X_COORD, 
+                Y_COORD, 
+                IMG_WIDTH, 
+                IMG_HEIGHT, 
+                imageArray[currentDisplayFrame]
+            );
+            u8g2.sendBuffer();
+            imageStatus[currentDisplayFrame] = 0;
+            currentDisplayFrame = (currentDisplayFrame + 1) % IMG_RACK;
+        }
+        // taskYIELD() (probably) only works with tasks with at least the same priority
+        vTaskDelay(1 / portTICK_PERIOD_MS);
+    }
+}
 
 // Codes that run once
 void setup() {
-    Serial.begin(921600);
-    Serial.println("Serial established");
+    Serial.setRxBufferSize(RX_BUFFER_SIZE);
+    Serial.begin(BAUD_RATE);
+    Serial.println("Serial started");
+    // Might speed up I2C display speed
     u8g2.setBusClock(1000000);
     u8g2.begin();
     u8g2.setDrawColor(0);
+
+    xTaskCreate(
+        serialProcess,
+        "serialProcess",
+        35000, // Arbitrary value
+        NULL,
+        0,
+        &serialHandle
+    );
+
+    xTaskCreate(
+        display,
+        "display",
+        25000, // Also arbitrary value
+        NULL,
+        1,
+        &displayHandle
+    );
+
+    // Wait until imageArray is filled before displaying
+    if (displayHandle != NULL) {
+        vTaskSuspend(displayHandle);
+    }
 }
 
 // Codes that run repeatedly
 void loop() {
-    /*
-    Help to signal setup() is completed on initial port connection, since 
-    controller's restart somehow takes too much time.
-    */
-    if (!isReadyToReceive) {
-        delay(DELAY);
-        Serial.write("F");
-    }
-
-    if (Serial.available()) {
-        isReadyToReceive = true;
-        // Store a byte chunk into byteArray
-        unsigned long xStart = micros();
-        if ((IMG_BYTES - bytePos) < BYTE_CHUNK) {
-            // Handle last chunk being smaller than expected
-            bytesRead = Serial.readBytesUntil('\n', imageArray + bytePos, BYTE_CHUNK);
-            // Signal computer to send new data
-            Serial.write("C");
-        }
-        else {
-            // Handle most other chunks
-            bytesRead = Serial.readBytes(imageArray + bytePos, BYTE_CHUNK);
-            // Signal computer to send new data
-            Serial.write("C");
-            // Add delay to wait for chunk till it arrives in full
-            // Slows down first chunk but speeds up last chunk
-            delay(DELAY+ DELAY_OFFSET);
-        }
-
-        bytePos += bytesRead;
-
-        unsigned long xEnd = micros();
-        Serial.print("\nRead: ");
-        Serial.println(xEnd - xStart);
-    }
-
-    // Display image when imageArray is full
-    if (bytePos >= IMG_BYTES) {
-        unsigned long displayStart = micros();
-        u8g2.drawXBM(X_COORD, Y_COORD, IMG_WIDTH, IMG_HEIGHT, imageArray);
-        u8g2.sendBuffer();
-        bytePos = 0;
-        unsigned long displayEnd = micros();
-        Serial.print("\nDisplay: ");
-        Serial.println(displayEnd - displayStart);
+    if (imageStatus[IMG_RACK-1] && displayHandle != NULL) {
+        vTaskResume(displayHandle);
     }
 }
-
-
